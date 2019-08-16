468f27a3c1c06e0390f33f527bbd2ede126338d3
Jason Boeshart <jboeshart@users.noreply.github.com>
2019-08-16 09:25:59 -0500

    separating out the before and after

---
+++ src/before/DroneDelivery.Common/Models/UserAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace DroneDelivery.Common.Models
+{
+    public class UserAccount
+    {
+        public UserAccount(string userid, string accountid)
+        {
+            UserId = userid;
+            AccountId = accountid;
+        }
+        public string UserId { get; }
+        public string AccountId { get; }
+    }
+}
---
+++ src/before/DroneDelivery.Common/Services/IDroneScheduler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DroneDelivery.Common.Models;
+
+namespace DroneDelivery.Common.Services
+{
+    public interface IDroneScheduler
+    {
+        Task<string> GetDroneIdAsync(Delivery deliveryRequest);
+    }
+}
